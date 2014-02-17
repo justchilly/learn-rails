2ac9cd542be9ae98a40760ebbd90f92a5bac0a94
Alexander Auchter <aauchter@gmail.com>
2014-02-17 15:12:01 -0500

    mailing list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 LearnRails::Application.routes.draw do
   # the only is a restriction that only allows the New and Create routes
   resources :contacts, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
 end
