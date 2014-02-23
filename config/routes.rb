5fbcbd5266ceb53f83c8bbd5c06097f1295f3105
Alexander Auchter <aauchter@gmail.com>
2014-02-22 19:56:38 -0500

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ LearnRails::Application.routes.draw do
   # the only is a restriction that only allows the New and Create routes
   resources :contacts, only: [:new, :create]
   resources :visitors, only: [:new, :create]
-  resources :users, only: [:new, :create]
+  # see railstutorial table 7.1 for all of the different possible routes
+  resources :users, only: [:new, :create, :show]
   root to: 'visitors#new'
 end
